9f8491e6b6acbb4c6f61363f1b409a76833d5b4b
Sergi Villanueva <sergi.villanueva@inscastellbisbal.net>
2026-04-09 13:08:46 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+// 1. Cambia el import de BrowserRouter a HashRouter
+import { HashRouter } from 'react-router-dom';
 
-createRoot(document.getElementById("root")!).render(<App />);
+// 2. En la parte del Render, envuelve tu App con HashRouter
+root.render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
