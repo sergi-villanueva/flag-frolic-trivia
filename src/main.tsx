ec6507ba0ba27ee74db0b6d7ce310de0670cf0f8
Sergi Villanueva <sergi.villanueva@inscastellbisbal.net>
2026-04-09 13:41:20 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-// Importamos HashRouter para que funcione en archivos locales
-import { HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'; // IMPORTANTE: HashRouter
 
 const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('No se encontró el elemento root');
 
-if (!rootElement) throw new Error('Failed to find the root element');
-
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <HashRouter>
       <App />
