694f682011661c6dc335c835cec81b18d35a0502
Sergi Villanueva <sergi.villanueva@inscastellbisbal.net>
2026-04-09 13:55:26 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { HashRouter } from 'react-router-dom'; // IMPORTANTE: HashRouter
+import { HashRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('No se encontró el elemento root');
 
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  // QUITAMOS StrictMode para evitar el doble renderizado que rompe la app
+  root.render(
     <HashRouter>
       <App />
     </HashRouter>
-  </React.StrictMode>
-);
+  );
+}
